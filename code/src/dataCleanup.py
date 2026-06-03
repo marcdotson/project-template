0f7564561d992ac1cb89dbd45e7ae028431a27f2
Stacy Bowler <Stacy.Bowler@sdl.usu.edu>
2026-06-02 21:54:36 -0600

    Break most the cleanup to a different file/function.
    Start making a model, plotting it and cleaning things up. Got VFs under 10 and model is looking good.

---
+++ code/src/dataCleanup.py
@@ -0,0 +1,163 @@
+import numpy as np
+import pandas as pd
+import polars as pl
+
+make_map = {
+  "vw": "volkswagen",
+  "mercedes": "mercedes-benz",
+  "mercedes-b": "mercedes-benz",
+  "landrover": "land rover",
+  "ford truck": "ford",
+  "ford tk": "ford",
+  "chev truck": "chevrolet",
+  "gmc truck": "gmc",
+  "mazda tk": "mazda",
+  "dodge tk": "dodge",
+  "hyundai tk": "hyundai",
+}
+
+body_map = {
+  "regular-cab": "regular cab",
+
+  "supercrew": "crew cab",
+  "supercab": "extended cab",
+  "crewmax cab": "crew cab",
+  "double cab": "crew cab",
+
+  "access cab": "extended cab",
+  "club cab": "extended cab",
+  "king cab": "extended cab",
+  "xtracab": "extended cab",
+
+  "cab plus": "extended cab",
+  "cab plus 4": "extended cab",
+
+  "e-series van": "van",
+  "promaster cargo van": "van",
+  "transit van": "van",
+  "ram van": "van",
+
+  "g sedan": "sedan",
+  "g coupe": "coupe",
+  "g convertible": "convertible",
+
+  "genesis coupe": "coupe",
+  "elantra coupe": "coupe",
+  "q60 coupe": "coupe",
+  "g37 coupe": "coupe",
+  "cts coupe": "coupe",
+  "cts-v coupe": "coupe",
+
+  "beetle convertible": "convertible",
+  "q60 convertible": "convertible",
+  "g37 convertible": "convertible",
+  "granturismo convertible": "convertible",
+  "granturismo convertible": "convertible",
+
+  "cts wagon": "wagon",
+  "cts-v wagon": "wagon",
+  "tsx sport wagon": "wagon",
+}
+
+def value_counts(df, col, **filters):
+  for filter_col, filter_val in filters.items():
+    df = df.filter(pl.col(filter_col) == filter_val)
+
+  return (
+    df.group_by(col)
+      .len()
+      .sort("len", descending=True)
+  )
+
+pl.Config.set_tbl_rows(-1)
+pl.Config.set_tbl_cols(-1)
+pl.Config.set_fmt_str_lengths(100)
+
+def cleanup_data(data):
+  # Drop missing values in key columns
+  df = data.drop_nulls(subset=['make', 'body', 'color', 'condition', 'year', 'saledate', 'sellingprice', 'state', 'transmission'])
+
+  # make, body, transmission, state, color need to be consistent and valid
+
+  # Clean 'make' column
+  df = df.with_columns(
+    pl.col("make")
+    .str.strip_chars()
+    .str.to_lowercase()
+  )
+
+  df = df.with_columns(
+    pl.col("make")
+    .replace(make_map)
+  )
+
+  # Clean up body
+  df = df.with_columns(
+    pl.col("body")
+      .str.strip_chars()
+      .str.to_lowercase()
+  )
+
+  df = df.with_columns(
+    pl.col("body").replace(body_map)
+  )
+
+  # Clean up transmission, convert any not automatic/manual to null and then drop them
+  df = df.with_columns(
+    pl.when(
+      ~pl.col("transmission")
+        .str.to_lowercase()
+        .is_in(["automatic", "manual"])
+    )
+    .then(None)
+    .otherwise(pl.col("transmission").str.to_lowercase())
+    .alias("transmission")
+  )
+
+  df = df.drop_nulls(subset=["transmission"])
+
+  # CLean up color
+  df = df.with_columns(
+    pl.when(pl.col("color") == "—")
+      .then(pl.lit("unknown"))
+      .otherwise(pl.col("color"))
+      .alias("color")
+  )
+
+  # Extract 'sale_year' from 'saledate'
+  df = df.with_columns(
+    sale_year=pl.col('saledate').str.extract(r'(\d{4})').cast(pl.Int64, strict=False)
+  )
+
+  # Calculate vehicle age: sale_year - year
+  df = df.with_columns(
+    vehicle_age=pl.col('sale_year') - pl.col('year')
+  )
+
+  # Filter out invalid ages
+  df = df.filter(pl.col('vehicle_age') >= 0)
+
+  # Potential states for project 
+  # │ in    ┆ 3933  │
+  # │ ne    ┆ 3914  │
+  # │ sc    ┆ 3882  │
+  # │ pr    ┆ 2445  │
+  # │ la    ┆ 2029  │
+  # │ ut    ┆ 1766  │
+  # │ ms    ┆ 1730  │
+  # │ hi    ┆ 1209  │
+  # │ or    ┆ 1049  │
+  # │ nm    ┆ 163   │
+
+  # Debug unique values, optionally filter by something
+  # print(value_counts(df, "make", state="ut"))
+  # print(value_counts(df, "body", state="ut"))
+  # print(value_counts(df, "transmission", state="ut"))
+  # print(value_counts(df, "state"))
+  # print(value_counts(df, "color", state="ut"))
+  # print(value_counts(df, "sale_year", state="ut"))
+  # print(value_counts(df, "condition", state='ut'))
+
+  # print(df.schema)
+
+  return df
--- code/src/trendAnalysis.py
+++ code/src/trendAnalysis.py
@@ -1,161 +1,15 @@
+import pandas as pd
 import polars as pl
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-make_map = {
-  "vw": "volkswagen",
-  "mercedes": "mercedes-benz",
-  "mercedes-b": "mercedes-benz",
-  "landrover": "land rover",
-  "ford truck": "ford",
-  "ford tk": "ford",
-  "chev truck": "chevrolet",
-  "gmc truck": "gmc",
-  "mazda tk": "mazda",
-  "dodge tk": "dodge",
-  "hyundai tk": "hyundai",
-}
-
-body_map = {
-  "regular-cab": "regular cab",
-
-  "supercrew": "crew cab",
-  "supercab": "extended cab",
-  "crewmax cab": "crew cab",
-  "double cab": "crew cab",
-
-  "access cab": "extended cab",
-  "club cab": "extended cab",
-  "king cab": "extended cab",
-  "xtracab": "extended cab",
-
-  "cab plus": "extended cab",
-  "cab plus 4": "extended cab",
-
-  "e-series van": "van",
-  "promaster cargo van": "van",
-  "transit van": "van",
-  "ram van": "van",
-
-  "g sedan": "sedan",
-  "g coupe": "coupe",
-  "g convertible": "convertible",
-
-  "genesis coupe": "coupe",
-  "elantra coupe": "coupe",
-  "q60 coupe": "coupe",
-  "g37 coupe": "coupe",
-  "cts coupe": "coupe",
-  "cts-v coupe": "coupe",
-
-  "beetle convertible": "convertible",
-  "q60 convertible": "convertible",
-  "g37 convertible": "convertible",
-  "granturismo convertible": "convertible",
-  "granturismo convertible": "convertible",
-
-  "cts wagon": "wagon",
-  "cts-v wagon": "wagon",
-  "tsx sport wagon": "wagon",
-}
-
-def value_counts(df, col, **filters):
-  for filter_col, filter_val in filters.items():
-    df = df.filter(pl.col(filter_col) == filter_val)
-
-  return (
-    df.group_by(col)
-      .len()
-      .sort("len", descending=True)
-  )
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from dataCleanup import cleanup_data
 
 raw_data = pl.read_csv('../../data/kaggle_car_prices.csv')
 
-pl.Config.set_tbl_rows(-1)
-pl.Config.set_tbl_cols(-1)
-pl.Config.set_fmt_str_lengths(100)
-
-# Drop missing values in key columns
-df = raw_data.drop_nulls(subset=['make', 'body', 'color', 'year', 'saledate', 'sellingprice', 'state', 'transmission'])
-
-# make, body, transmission, state, color need to be consistent and valid
-
-# Clean 'make' column
-df = df.with_columns(
-  pl.col("make")
-  .str.strip_chars()
-  .str.to_lowercase()
-)
-
-df = df.with_columns(
-  pl.col("make")
-  .replace(make_map)
-)
-
-# Clean up body
-df = df.with_columns(
-  pl.col("body")
-    .str.strip_chars()
-    .str.to_lowercase()
-)
-
-df = df.with_columns(
-  pl.col("body").replace(body_map)
-)
-
-# Clean up transmission, convert any not automatic/manual to null and then drop them
-df = df.with_columns(
-  pl.when(
-    ~pl.col("transmission")
-      .str.to_lowercase()
-      .is_in(["automatic", "manual"])
-  )
-  .then(None)
-  .otherwise(pl.col("transmission").str.to_lowercase())
-  .alias("transmission")
-)
-
-df = df.drop_nulls(subset=["transmission"])
-
-# CLean up color
-df = df.with_columns(
-  pl.when(pl.col("color") == "—")
-    .then(pl.lit("unknown"))
-    .otherwise(pl.col("color"))
-    .alias("color")
-)
-
-# Extract 'sale_year' from 'saledate'
-df = df.with_columns(
-  sale_year=pl.col('saledate').str.extract(r'(\d{4})').cast(pl.Int64, strict=False)
-)
-
-# Calculate vehicle age: sale_year - year
-df = df.with_columns(
-  vehicle_age=pl.col('sale_year') - pl.col('year')
-)
-
-# Filter out invalid ages
-df = df.filter(pl.col('vehicle_age') >= 0)
-
-# Potential states for project 
-# │ in    ┆ 3933  │
-# │ ne    ┆ 3914  │
-# │ sc    ┆ 3882  │
-# │ pr    ┆ 2445  │
-# │ la    ┆ 2029  │
-# │ ut    ┆ 1766  │
-# │ ms    ┆ 1730  │
-# │ hi    ┆ 1209  │
-# │ or    ┆ 1049  │
-# │ nm    ┆ 163   │
-
-# Debug unique values, optionally filter by something
-# print(value_counts(df, "make", state="ut"))
-# print(value_counts(df, "body", state="ut"))
-# print(value_counts(df, "transmission", state="ut"))
-# print(value_counts(df, "state"))
-# print(value_counts(df, "color", state="ut"))
+df = cleanup_data(raw_data)
 
 # Setup a 1x3 grid for the 3 plots
 fig, axes = plt.subplots(1, 3, figsize=(18, 6))
@@ -192,3 +46,114 @@ plt.tight_layout()
 plt.savefig('trend_analysis.png')
 print("Plots successfully generated and saved to trend_analysis.png")
 plt.show()
+
+y = df["sellingprice"]
+
+features = [
+    "vehicle_age",
+    "odometer",
+    "condition",
+    # "mmr", # We need to remove mmr as it's already an estimate for the price of the vehicle
+    # "make", # make is captured in model since a model like z3 is only for BMW
+    "model",
+    # "body", # Body tends to be captured by the model as well
+    # "transmission", # insignificant
+    # "color", # color is not statistically significant and mostly just added noise
+]
+
+train_df = df.filter(pl.col("state") == "ut")
+test_df  = df.filter(pl.col("state") == "ms")
+
+train_pd = train_df.to_pandas()
+test_pd = test_df.to_pandas()
+
+# Tricky, some models are very rare and add a lot of extra variables to look at. With so few items it may start to memorize the data as well,
+# so we will combine rare models together, while preserving the make still so like BMW_Other.
+threshold = 10
+
+model_counts = (
+    train_pd.groupby(['make', 'model'])
+             .size()
+             .reset_index(name='count')
+)
+
+common_models = set(
+    zip(
+        model_counts.loc[model_counts['count'] >= threshold, 'make'],
+        model_counts.loc[model_counts['count'] >= threshold, 'model']
+    )
+)
+
+def collapse_model(row):
+  if (row['make'], row['model']) in common_models:
+    return row['model']
+  return f"{row['make']}_Other"
+
+train_pd['model'] = train_pd.apply(collapse_model, axis=1)
+test_pd['model'] = test_pd.apply(collapse_model, axis=1)
+
+X_train = pd.get_dummies(
+    train_pd[features],
+    drop_first=True
+)
+
+X_test = pd.get_dummies(
+    test_pd[features],
+    drop_first=True
+)
+
+X_test = X_test.reindex(
+    columns=X_train.columns,
+    fill_value=0
+)
+
+X_train = sm.add_constant(X_train)
+X_test = sm.add_constant(X_test)
+
+y_train = np.log(train_pd["sellingprice"])
+
+X_train = X_train.astype(float)
+X_test = X_test.astype(float)
+
+y_train = y_train.astype(float)
+
+
+print(X_train.dtypes)
+
+print(X_train.dtypes.value_counts())
+
+fit_log = sm.OLS(y_train, X_train).fit()
+
+print(fit_log.summary())
+
+y_test = np.log(test_pd["sellingprice"])
+
+y_test = y_test.astype(float)
+
+predictions = fit_log.predict(X_test)
+
+residuals = y_test - predictions
+
+plt.scatter(predictions, residuals)
+plt.axhline(0)
+plt.xlabel("Fitted Values")
+plt.ylabel("Residuals")
+plt.title("Residuals vs Fitted")
+plt.savefig('ResidualsVsFitted.png')
+plt.show()
+
+sm.qqplot(residuals, line='45')
+
+plt.title("Q-Q Plot")
+plt.savefig('qqPlot.png')
+plt.show()
+
+vif_data = pd.DataFrame({
+    "Variable": X_train.columns,
+    "VIF": [variance_inflation_factor(X_train.values, i)
+            for i in range(X_train.shape[1])]
+})
+
+print(
+    vif_data.sort_values("VIF", ascending=False)
+)
